d6d6faee7f01725d386f29dcb2398a5dc12a645d
FrydmanPiotr <98648816+FrydmanPiotr@users.noreply.github.com>
2022-06-30 15:42:25 +0200

    Add files via upload
    Utworzenie strony z blogami użytkownika:
    Szablony: wszystkie blogi, konkretny blog użytkownika
    Widoki: blogi, blog użytkownika.
    Adresy URL: blogi, blog użytkownika.

--- views.py
+++ views.py
@@ -1,5 +1,21 @@
 from django.shortcuts import render
 
+from .models import Blog
+
 def home(request):
     """Strona główna aplikacji MyBlog."""
     return render(request, 'my_blog/home.html')
+
+def blogs(request):
+    """Blogi użytkownika."""
+    blogs = Blog.objects.order_by('date_added')
+    context = {'blogs': blogs}
+    return render(request, 'my_blog/blogs.html', context)
+
+def blog(request, blog_id):
+    """Wyświetla blog użytkownika i opublikowane na nim posty."""
+    blog = Blog.objects.get(id=blog_id)
+    posts = blog.post_set.order_by('-date_added')
+    context = {'blog': blog, 'posts': posts}
+    return render(request, 'my_blog/blog.html', context)
+    
