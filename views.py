0d7f18022150943746eeffa3c65af4d799b14e0f
FrydmanPiotr <98648816+FrydmanPiotr@users.noreply.github.com>
2022-07-02 10:13:21 +0200

    Add files via upload
    Utworzenie stron przeznaczonych do dodania nowego bloga i nowego postu.

--- views.py
+++ views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 from .models import Blog
+from .forms import BlogForm, PostForm
 
 def home(request):
     """Strona główna aplikacji MyBlog."""
@@ -18,4 +19,41 @@ def blog(request, blog_id):
     posts = blog.post_set.order_by('-date_added')
     context = {'blog': blog, 'posts': posts}
     return render(request, 'my_blog/blog.html', context)
+
+def new_blog(request):
+    """Utwórz nowy blog."""
+    if request.method != 'POST':
+        # Nie zostały przekazane żadne dane. Utwórz pusty formularz.
+        form = BlogForm()
+    else:
+        # Przekazano dane za pomocą żądania POST. Przetwórz te dane.
+        form = BlogForm(data=request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('my_blog:blogs')
+
+    # Wyświetla pusty formularz.
+    context = {'form': form}
+    return render(request, 'my_blog/new_blog.html', context)
+
+def new_post(request, blog_id):
+    """Tworzenie nowego postu na blogu."""
+    blog = Blog.objects.get(id=blog_id)
+
+    if request.method != 'POST':
+        # Nie zostały przekazane żadne dane. Utwórz pusty formularz.
+        form = PostForm()
+    else:
+        # Przekazano dane za pomocą żądania POST. Przetwórz te dane.
+        form = PostForm(data=request.POST)
+        if form.is_valid():
+            new_post = form.save(commit=False)
+            new_post.blog = blog
+            new_post.save()
+            return redirect('my_blog:blog', blog_id=blog_id)
+
+    # Wyświetla pusty formularz.
+    context = {'blog': blog, 'form': form}
+    return render(request, 'my_blog/new_post.html', context)
+
     
