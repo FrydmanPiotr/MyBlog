5a53723eed4556dbac8894bc91b45096698697af
FrydmanPiotr <98648816+FrydmanPiotr@users.noreply.github.com>
2022-07-03 12:34:52 +0200

    Add files via upload
    Dodanie stron umożliwiających edycję postu i edycję nazwy bloga.

--- urls.py
+++ urls.py
@@ -14,6 +14,10 @@ urlpatterns = [
     path('blogs/(<int:blog_id>)/', views.blog, name='blog'),
     # Strona przeznaczona do utworzenia nowego bloga.
     path('new_blog/', views.new_blog, name='new_blog'),
+    # Strona przeznaczona do edycji nazwy bloga.
+    path('edit_blog_title/<int:blog_id>/', views.edit_blog_title, name='edit_blog_title'),
     # Strona przeznaczona do utworzenia nowego postu.
     path('new_post/(<int:blog_id>)/', views.new_post, name='new_post'),
+    # Strona przeznaczona do edycji postu.
+    path('edit_post/<int:post_id>/', views.edit_post, name='edit_post'),
 ]
